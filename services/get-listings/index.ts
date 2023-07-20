a46334a4187fc2abcc3bb938fc9da0cedd99a12a
Jack Corton <jack.corton@googlemail.com>
2023-07-20 22:42:58 +0100

    create service

---
+++ services/get-listings/index.ts
@@ -0,0 +1,33 @@
+import { api } from '../../config/api'
+
+export const getListings = async ({
+    query,
+    pageNumber,
+    size,
+    additionalPages,
+    sort }: Params) => {
+
+    //access secret api key for security
+    const apiKey = process.env.NEXT_PUBLIC_API_KEY;
+
+    //can access HTTP verbs through the api object methods
+    const { data } = await api.post(
+        `interviews/listings?apikey=${apiKey}`,
+        {
+            query,
+            pageNumber,
+            size,
+            additionalPages,
+            sort
+        }
+    )
+    return data;
+}
+
+interface Params {
+    query: string
+    pageNumber: number
+    size: number
+    additionalPages: number
+    sort: number
+}

ff370348efaaeb376c83014699d1e0bd130679e1
Jack Corton <jack.corton@googlemail.com>
2023-07-20 22:43:08 +0100

    make colours constant

---
+++ styles/colors.ts
@@ -0,0 +1,7 @@
+//easy to maintain and change colours throughout the site
+export const colors = {
+    primary: "#439A86",
+    secondary: "#DBC2CF",
+    light: "#BCD8C1",
+    dark: "#3A3335",
+} 

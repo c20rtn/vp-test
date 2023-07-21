f6728e27f150e23326e96e3eb8920e1f1d38bc4f
Jack Corton <jack.corton@googlemail.com>
2023-07-21 08:41:48 +0100

    facet additions

--- components/molecules/filterBox.tsx
+++ components/molecules/filterBox.tsx
@@ -1,13 +1,29 @@
 import { AccordionItem, AccordionButton, AccordionIcon, AccordionPanel, Box } from '@chakra-ui/react'
-import React from 'react'
-import { Facet } from '../../entities'
+import React, { Dispatch, SetStateAction } from 'react'
+import { Facet, Option } from '../../entities'
 import { Checkbox } from '../atoms/checkbox'
 
 interface Props {
-    facet: Facet
+    facet: Facet,
+    facetFilters: {}
+    setFacetFilters: Dispatch<SetStateAction<{}>>
 }
 
-export const FilterBox = ({ facet }: Props) => {
+export const FilterBox = ({ facet, facetFilters, setFacetFilters }: Props) => {
+
+    const addRemoveFacet = (option: Option, e: React.ChangeEvent<HTMLInputElement>) => {
+        if(e.currentTarget.checked){ //add the facet
+            const newFacet = {
+                identifier: option.identifier,
+                value: option.value
+            }
+            setFacetFilters(
+                {...facetFilters, [facet.identifier]:(facetFilters as any)["categories"]?.concat(newFacet)}      
+            )
+            return
+        }
+    }
+    
     return (
         <AccordionItem id={facet.identifier} mt={"2px"} bg="gray.100" border="1px">
             <h2>
@@ -20,7 +36,7 @@ export const FilterBox = ({ facet }: Props) => {
             </h2>
             <AccordionPanel w="full">
                 {facet.options.map((option) =>
-                    <Checkbox option={option} key={option.displayValue} />
+                    <Checkbox option={option} key={option.displayValue} onChange={e => {}}/>
                 )}
             </AccordionPanel>
         </AccordionItem>

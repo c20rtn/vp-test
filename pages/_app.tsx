654262a4afb8b87aa1c2504d348fe07ee7037223
Jack Corton <jack.corton@googlemail.com>
2023-07-23 10:58:50 +0100

    adding bg and font

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,25 @@
-import { ChakraProvider } from '@chakra-ui/react'
-import '../styles/globals.css'
+import { ChakraProvider, extendTheme } from '@chakra-ui/react'
 import type { AppProps } from 'next/app'
 import { QueryClient, QueryClientProvider } from 'react-query'
 
 const queryClient = new QueryClient()
 
+const theme = extendTheme({
+  styles: {
+    global: () => ({
+      body: {
+        bg:"gray.100",
+      }
+    })
+  },
+  fonts: {
+    body: `'Helvetica', sans-serif`,
+  },
+})
+
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <ChakraProvider>
+    <ChakraProvider theme={theme}>
       <QueryClientProvider client={queryClient}>
         <Component {...pageProps} />
       </QueryClientProvider>
